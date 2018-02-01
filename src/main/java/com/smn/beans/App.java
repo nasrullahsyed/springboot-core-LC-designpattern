c4766e7738926b74a94c832023c0cf2079740aff
Nasrullah <syed.nasru@gmail.com>
2018-02-01 18:16:58 +0530

    ADDED A COMMENT

--- src/main/java/com/smn/beans/App.java
+++ src/main/java/com/smn/beans/App.java
@@ -16,6 +16,7 @@ public class App {
 		 // getBean
 		 ctx = SpringApplication.run(App.class, args);
 		 flipkart = ctx.getBean("flipkart",Flipkart.class);
+		 // for testing...
 		 System.out.println(flipkart.Shopping(new String[]{"Shirt"," Pant"}));
 
 	}
